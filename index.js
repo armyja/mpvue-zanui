fd339044e48ba3ae17189fcdb3c53f27934767d9
Jeffrey Wang <546323405@qq.com>
2018-03-16 21:23:01 +0800

    add entry for npm

---
+++ index.js
@@ -0,0 +1,26 @@
+import Actionsheet from './src/components/zan/actionsheet'
+import Dialog from './src/components/zan/dialog'
+import Field from './src/components/zan/field'
+import NoticeBar from './src/components/zan/noticebar'
+import Select from './src/components/zan/select'
+import Stepper from './src/components/zan/stepper'
+import Switch from './src/components/zan/switch'
+import Tab from './src/components/zan/tab'
+import Toast from './src/components/zan/toast'
+import TopTips from './src/components/zan/toptips'
+import { getComponentByTag, extractComponentId } from './src/utils/helper'
+
+export {
+  Actionsheet,
+  Dialog,
+  Field,
+  NoticeBar,
+  Select,
+  Stepper,
+  Switch,
+  Tab,
+  Toast,
+  TopTips,
+  getComponentByTag,
+  extractComponentId
+}
